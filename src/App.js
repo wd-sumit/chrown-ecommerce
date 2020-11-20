03e83a9ba346ef2325c4e7c8c474b13059c64f37
sumit <wdsumit.pal@gmail.com>
2020-11-20 15:24:49 +0530

    added redux, and implemented userReducer and userActions to header and app component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
+import { connect } from 'react-redux';
+
 import './App.css';
 
+import {setCurrentUser} from './redux/user/user.action';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shopPage.component';
 import SignInAndSignUpPage from './pages/auth/auth.component';
@@ -9,18 +12,15 @@ import Header from './component/header/header.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
-  state = {
-    currentUser: null
-  }
-
   unsubscribeFromAuth = null;
 
   componentDidMount() {
+    const { setCurrentUser } = this.props;
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if(userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
         userRef.onSnapshot(snapShot => {
-          this.setState({
+          setCurrentUser({
             currentUser: {
               id: snapShot.id,
               ...snapShot.data(),
@@ -28,8 +28,7 @@ class App extends React.Component {
           });
         });
       }
-      this.setState({currentUser: userAuth}, () => {
-      });
+      setCurrentUser(userAuth);
     });
   }
 
@@ -40,7 +39,7 @@ class App extends React.Component {
   render(){
     return (
       <div>
-        <Header currentUser={this.state.currentUser}/>
+        <Header />
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
@@ -51,4 +50,8 @@ class App extends React.Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user)),
+});
+
+export default connect(null, mapDispatchToProps)(App);
