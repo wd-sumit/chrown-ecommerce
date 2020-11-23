fe396464d8618332827b7da81daf455c5148513d
sumit pal <wdsumit.pal@gmail.com>
2020-11-24 00:15:52 +0530

    implemented cart functionality added state to cart and implemented add quanity for similiar items in cart

--- src/component/header/header.component.jsx
+++ src/component/header/header.component.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import { connect } from 'react-redux';
 
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+import CartIcon from '../cart-icon/cart-icon.component';
 import { auth } from '../../firebase/firebase.utils';
 import { ReactComponent as Logo} from '../../assets/svg/crown.svg';
 
 import './header.styles.scss';
 
-const Header = ({currentUser}) => (
+const Header = ({currentUser, hidden}) => (
   <div className='header'>
     <Link className='logo-container' to='/'>
       <Logo className='logo' />
@@ -29,13 +31,15 @@ const Header = ({currentUser}) => (
           </Link>
         )
       }
-      
+      <CartIcon />
+      {hidden ? null : <CartDropdown />}
     </div>
   </div>
 );
 
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser,
+const mapStateToProps = ({user : {currentUser}, cart: {hidden}}) => ({
+  currentUser,
+  hidden
 });
 
 export default connect(mapStateToProps)(Header);
--- src/pages/shop/shopPage.component.jsx
+++ src/pages/shop/shopPage.component.jsx
@@ -10,7 +10,7 @@ class ShopPage extends React.Component {
   render() {
     const { collections } = this.state;
     return (
-      <div clasName='shop-page'>
+      <div className='shop-page'>
         {
           collections.map(({id, ...otherCollectionProps}) => (
             <CollectionPreview key={id} {...otherCollectionProps}/>
